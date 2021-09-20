bc7d30df1cf6919d4171e82540e79cd55285edcd
bakseoyong <qwerkdud1999@gmail.com>
2021-09-20 11:24:43 +0900

    3.4 Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,13 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
 
+call.hidden = true;
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
     try {
@@ -18,7 +21,7 @@ async function getCameras() {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
-            if(currentCamera.label === camera.label){
+            if (currentCamera.label === camera.label) {
                 option.selected = true;
             }
             camerasSelect.appendChild(option);
@@ -47,8 +50,6 @@ async function getMedia(deviceId) {
         console.log(e);
     }
 }
-
-getMedia();
 
 function handleMuteClick() {
     myStream
@@ -83,3 +84,28 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia() {
+welcome.hidden = true;
+call.hidden = false;
+getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+event.preventDefault();
+const input = welcomeForm.querySelector("input");
+socket.emit("join_room", input.value, startMedia);
+roomName = input.value;
+input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", () => {
+console.log("someone joined");
+});
